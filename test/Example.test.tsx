9f3d6a2efec83295c962d1300488fb835801a579
Ari Perkkiö <ari.perkkio@gmail.com>
2026-05-15 13:50:17 +0300

    ci: upload report

--- test/Example.test.tsx
+++
@@ -1,7 +0,0 @@
-import { test } from "vitest";
-
-test("example test", async () => {
-  const heading = document.createElement("h1");
-  heading.textContent = "Hello, world!";
-  document.body.appendChild(heading);
-});
